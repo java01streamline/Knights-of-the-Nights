cfcd24fe98af58eeebe8faa75725597608c2c580
1 <1@Streamline-O09>
2019-03-10 18:06:20 +0200

    AAA

--- src/zametki/zametki.java
+++ src/zametki/zametki.java
@@ -8,29 +8,29 @@ ID of all entities [123456]:
 
 materials:
 
-00 wood
-01 stone
-02 metal
+00 wood*
+01 stone*
+02 metal*
 03 steel
 04 bone
 05 diamonds
-06 coal
+06 coal*
 07 brick
 08 clay
 09 marble
-10 
-11 
-12 
-13 
-14 
-15 
-16 
-17 
-18 
-19 
-20 
-21 
-22 
+10 dirt
+11 mud
+12 water
+13 planks*
+14 glass
+15 sand
+16 dust*
+17 obsidian
+18 quartz
+19 flint
+20 leather
+21 wool
+22 silk
 23 
 24 
 25 
