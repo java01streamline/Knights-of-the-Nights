8d1c2c3bf9e5dd6eb891a92fa9e301b4192e0e99
2 <2@DESKTOP-LF51703>
2019-03-15 18:45:39 +0200

    Всё красиво и удобно; Улучшаю менюшку

--- src/zametki/zametki.java
+++ src/zametki/zametki.java
@@ -1,8 +1,9 @@
 /*
-ID of all entities [123456]:
+ID of all entities [12345678]:
 1 - level of entity
-2,3 - concret type
-4 - ent. type
-5 - atitude to you
-6 - image to show
+2 - type
+3 - image now
+4 - antitude to you
+5,6 - conc type
+7,8 - material
 */
