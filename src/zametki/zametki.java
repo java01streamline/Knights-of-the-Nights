8c2539eff27a30eb1df909b6e64b4867400bc318
2 <2@DESKTOP-LF51703>
2019-03-15 18:37:57 +0200

    Всё красиво и удобно; Улучшаю менюшку

--- src/zametki/zametki.java
+++ src/zametki/zametki.java
@@ -5,39 +5,4 @@ ID of all entities [123456]:
 4 - ent. type
 5 - atitude to you
 6 - image to show
-
-materials:
-
-00 wood*
-01 stone*
-02 metal*
-03 steel
-04 bone
-05 diamonds
-06 coal*
-07 brick
-08 clay
-09 marble
-10 dirt
-11 mud
-12 water
-13 planks*
-14 glass
-15 sand
-16 dust*
-17 obsidian
-18 quartz
-19 flint
-20 leather
-21 wool
-22 silk
-23 
-24 
-25 
-26 
-27 
-28 
-29 
-30
-
 */
